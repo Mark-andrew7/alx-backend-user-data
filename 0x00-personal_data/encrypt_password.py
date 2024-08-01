c495601497afa23f6ab3722e9720aec611d03cfa
Mark-andrew7 <kamaumark19@gmail.com>
2024-08-01 08:51:48 +0300

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,7 @@ import bcrypt
 def hash_password(password: str) -> str:
     """Returns a hashed password."""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+def is_valid(hashed_password: str, password: str) -> bool:
+    """Returns a boolean."""
+    return bcrypt.checkpw(password.encode(), hashed_password.encode())
