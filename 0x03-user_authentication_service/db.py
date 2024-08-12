d4378b83924f1b43cea7d749dcaed0977fdedade
Mark-andrew7 <kamaumark19@gmail.com>
2024-08-12 12:20:51 +0300

    Update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -52,3 +52,15 @@ class DB:
         except InvalidRequestError as e:
             raise InvalidRequestError(f"Invalid query arguments provided: {e}")
         
+    def update_user(self, user_id:int, **kwargs) -> None:
+        """
+        Update a user's attributes
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError(f"Invalid attribute: {key}")
+            setattr(user, key, value)
+
+                    
+        
