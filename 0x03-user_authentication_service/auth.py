d91d5b401efea00e8c2bf45d6c15cfa5f242376f
Mark-andrew7 <kamaumark19@gmail.com>
2024-08-13 15:16:49 +0300

    User registration applied

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -29,12 +29,14 @@ class Auth:
         """
         Register a user
         """
-        user = self._db.find_user_by(email=email)
-        if user:
-            raise ValueError(f"User {email} already exists")
-        hashed_password = _hash_password(password)
-        return self._db.add_user(email, hashed_password)
-    
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {user.email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
+
     def valid_login(self, email: str, password: str) -> bool:
         """
         Validate a user's login
@@ -43,7 +45,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             return False
-        
-        user_password  = user.hashed_password
+
+        user_password = user.hashed_password
         if bcrypt.checkpw(password.encode('utf-8'), user_password):
             return True
